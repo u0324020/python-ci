27a1cb00fdfe850151b4f556f313f81549846a73
jane-yc-chen <jane_yc_chen@trendmicro.com>
2021-08-18 22:04:18 +0800

    add testcase

--- test_sumtwo.py
+++ test_sumtwo.py
@@ -1,14 +1,19 @@
 from main import sumtwo
 
+def test_one():
+    input = 1
+    expected = 2
+    result = sumtwo(input)
+    assert result == expected
+
 def test_two():
     input = 2
     expected = 4
     result = sumtwo(input)
     assert result == expected
 
-def test_two():
+def test_ten():
     input = 10
     expected = 20
     result = sumtwo(input)
     assert result == expected
-    
